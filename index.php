be9cfc6b6d307f7b69a8a6da17dd77be32b3476a
ElDiGio <57814202+eldigio@users.noreply.github.com>
2022-11-22 12:47:26 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,14 +1,7 @@
 <?php
-/*   include_once "utils.php"; */
+  //include_once "utils.php";
 
   $htmlColori = "";
-
-  $word_count = array_count_values(file("colori.txt"));
-  arsort($word_count);
-
-  foreach ($word_count as $key => $value) {
-    $htmlColori .= "<tr><td>$key</td><td>$value</td></tr>";
-  }
 
 
 ?>
@@ -22,24 +15,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous" defer></script>
-    <title>Colori</title>
+    <title>index</title>
   </head>
 
-  <body>
+  <body class="bg-dark text-light">
 
     <div class="container">
-    <h1>Occorrenze Colori</h1>
-      <table class="table table-striped">
+    <!-- <h1 class="my-4">Occorrenze Colori</h1>
+      <table class="table table-striped table-dark">
         <thead>
           <tr>
             <th scope="col">Nome Colore</th>
             <th scope="col">Occorrenze</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody class="text-capitalize">
           <?= $htmlColori ?>
         </tbody>
-      </table>
+      </table> -->
     </div>
 
   </body>
