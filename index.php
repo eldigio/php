142f17555b8f582f237d6471d4fd0528b64784f8
ElDiGio <57814202+eldigio@users.noreply.github.com>
2022-11-24 23:05:34 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,40 +1,105 @@
 <?php
-  //include_once "utils.php";
 
-  $htmlColori = "";
+include_once "utils.php";
 
+$html = "";
+$dataHtml = date("Y-m-d");
+$a_selected = "";
+$b_selected = "";
+$c_selected = "";
+$italia_selected = "";
+$francia_selected = "";
+$usa_selected = "";
+$giappone_selected = "";
+
+if (isset($_POST["dataNascita"]) && isset($_POST["selectPatente"]) && isset($_POST["nazione"])) {
+  $dataNascita = $_POST["dataNascita"];
+  $dataHtml = $dataNascita;
+  $tipoPatente = $_POST["selectPatente"];
+  $nazione = $_POST["nazione"];
+
+  if ($patente[$nazione][$tipoPatente] <= person_age($dataNascita)) {$html = "$tipoPatente: si";}
+  else $html = "$tipoPatente: no";
+  
+  switch ($tipoPatente) {
+    case "A":
+      $a_selected = "selected";
+      break;
+    case "B":
+      $b_selected = "selected";
+      break;
+    case "C":
+      $c_selected = "selected";
+      break;
+  }
+
+  switch ($nazione) {
+    case "Italia":
+      $italia_selected = "selected";
+      break;
+    case "Francia":
+      $francia_selected = "selected";
+      break;
+    case "USA":
+      $usa_selected = "selected";
+      break;
+    case "Giappone":
+      $giappone_selected = "selected";
+      break;
+  }
+}
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-
   <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous" defer></script>
-    <title>index</title>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+      crossorigin="anonymous"
+    />
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+      crossorigin="anonymous"
+      defer
+    ></script>
+    <title>form</title>
   </head>
 
-  <body class="bg-dark text-light">
-
+  <body>
     <div class="container">
-    <!-- <h1 class="my-4">Occorrenze Colori</h1>
-      <table class="table table-striped table-dark">
-        <thead>
-          <tr>
-            <th scope="col">Nome Colore</th>
-            <th scope="col">Occorrenze</th>
-          </tr>
-        </thead>
-        <tbody class="text-capitalize">
-          <?= $htmlColori ?>
-        </tbody>
-      </table> -->
+      <h1>Titolo della pagina</h1>
+      <form action="" method="post">
+        <div class="mb-3">
+          <label class="form-label">Data di nascita</label>
+          <input type="date" name="dataNascita" value="<?= $dataHtml ?>" class="form-control" />
+        </div>
+        <div class="mb-3">
+          <label class="form-label">Tipo di patente</label>
+          <select name="selectPatente" class="form-select">
+            <option value="A"<?= $a_selected ?>>A</option>
+            <option value="B"<?= $b_selected ?>>B</option>
+            <option value="C"<?= $c_selected ?>>C</option>
+          </select>
+        </div>
+        <div class="mb-3">
+          <label class="form-label">Nazionalità</label>
+          <select class="form-select mb-3" name="nazione">
+            <option value="Italia"<?= $italia_selected ?>>Italia</option>
+            <option value="Francia"<?= $francia_selected ?>>Francia</option>
+            <option value="USA"<?= $usa_selected ?>>USA</option>
+            <option value="Giappone"<?= $giappone_selected ?>>Giappone</option>
+          </select>
+        </div>
+        <input type="submit" class="btn btn-primary mb-3" name="btn" value="Calcola" />
+      </form>
+      <pre><?= $html ?></pre>
     </div>
-
   </body>
-
 </html>
--- utils.php
+++ utils.php
@@ -2,9 +2,42 @@
 
 $gender = ["M" => "Maschio", "F" => "Femmina"];
 
-function dateFormat (string $date, string $format) {
-  return date_format(date_create_from_format('Y-m-d', $date), $format);
+// function dateFormat (string $date, string $format) {
+//   return date_format(date_create_from_format("Y-m-d", $date), $format);
+// }
+
+function format_date (string $date) {
+  return date("d-M-Y", strtotime($date));
 }
+
+function person_age (string $dataNascita) {
+  $today = date("Y-m-d");
+  $diff = date_diff(date_create($dataNascita), date_create($today));
+  return $diff->format("%y");
+}
+
+$patente = [
+  "Italia" => [
+    "A" => 16,  //2006
+    "B" => 18,  //2004
+    "C" => 21   //2001
+  ],
+  "Francia" => [
+    "A" => 18,  //2004
+    "B" => 19,  //2003
+    "C" => 20   //2002
+  ],
+  "USA" => [
+    "A" => 16,  //2006
+    "B" => 21,  //2001
+    "C" => 20   //2002
+  ],
+  "Giappone" => [
+    "A" => 15,  //2007
+    "B" => 17,  //2005
+    "C" => 18   //2004
+  ],
+];
 
 $comuni = [
   "ABR" => "Abruzzo",
@@ -32,5 +65,3 @@ $comuni = [
 function format_num (int $num) {
   return number_format($num, 2, ",", "'");
 }
-
-?>
